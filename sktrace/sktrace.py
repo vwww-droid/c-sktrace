7926a7043a97993c0092cb6b4421b0fa7061d65d
vwvw <vwvw2025@outlook.com>
2025-10-18 16:21:08 +0800

    feat: 改为 C 实现
    1. C before/after callout
    2. 添加数据结构寄存器快照
    3. c 不进行数据处理,使用python异步处理

--- sktrace/sktrace.py
+++ sktrace/sktrace.py
@@ -86,9 +86,7 @@ def main():
     else:
         config["payload"]["symbol"] = args.interceptor
     
-    # 根据参数选择连接设备的方式
     if args.host:
-        # 连接到远程 frida-server
         device = frida.get_device_manager().add_remote_device(args.host)
     else:
         # 默认使用 USB 设备
